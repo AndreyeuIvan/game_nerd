b32ad8554fa70666cb283da77404782f165d30d1
AndreyeuIvan <58092467+AndreyeuIvan@users.noreply.github.com>
2022-02-21 17:19:36 +0300

    Drf (#10)
    * Launch time
    
    * change settings
    
    * start
    
    * start
    
    * start1
    
    * created fixtures in management/nerdy
    
    * Delete .env
    
    * hello heroku
    
    * fixed accordingly with python way
    
    * changed html
    
    * created drf

---
+++ games/serializers.py
@@ -0,0 +1,14 @@
+from platform import platform
+from rest_framework import serializers
+from games.models import Game
+
+
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+    genres = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
+    platforms = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
+    tweets = serializers.SlugRelatedField(slug_field='text', read_only=True, many=True)
+    likes = serializers.SlugRelatedField(slug_field='username', read_only=True, many=True)
+    class Meta:
+        model = Game
+        fields = ['name', 'slug', 'id', 'genres', 'platforms', 'tweets', 'likes']
+        lookup_field = 'name'
--- games/urls.py
+++ games/urls.py
@@ -1,12 +1,19 @@
-from django.urls import path, re_path
+from django.urls import path, include
 
-from games.views import main, detail, favorite, like
+from rest_framework import routers
+
+from games.views import main, detail, favorite, like, GameViewSet
+
 
 app_name = "games"
+
+router = routers.DefaultRouter()
+router.register(r'viewset', GameViewSet)
 
 urlpatterns = [
     path("", main, name="main"),
     path("like/", like, name="like"),
     path("detail/<int:id>/", detail, name="detail"),
     path("favorite/", favorite, name="favorite"),
+    path('api/', include(router.urls)),
 ]
