eb059eff6c01b3a7544113962fc68d517f7307e1
AndreyeuIvan <58092467+AndreyeuIvan@users.noreply.github.com>
2022-02-24 00:23:04 +0300

    migrations fixed (#15)

--- game_nerd/settings.py
+++ game_nerd/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "game_nerd.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-"""
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -103,7 +103,7 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
+"""
 # db_from_env = dj_database_url.config(conn_max_age=600)
 # DATABASES['default'].update(db_from_env)
 
