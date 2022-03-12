97457d745200323ddc0845317d7245582cfba7df
AndreyeuIvan <58092467+AndreyeuIvan@users.noreply.github.com>
2022-03-12 16:22:02 +0300

    Revert "Tests" (#18)
    * configured docker
    
    * migrations fixed (#15)
    
    * updated docker file, added migrations
    
    * configured docker (#13)
    
    * configured docker
    
    * updated docker file, added migrations
    
    * tests created
    
    * Delete .env
    
    * search added
    
    * Revert "Tests"
    
    * Delete .env

--- games/tests.py
+++ games/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from unittest import TestCase
+from unittest.mock import patch
+from games import views
 
-# Create your tests here.
+
+class TestGame(TestCase):
+    @patch('games.views.main')
+    def test_games_main(self, MockGame):
+        game = MockGame()
+
+        game.return_value = [
+            {
+                "name": "Scarlet Blade",
+                "slug": "scarlet-blade",
+                "id": 1977,
+                "genres": [
+                    "Role-playing (RPG)",
+                    "Hack and slash/Beat 'em up"
+                ],
+                "platforms": [
+                    "PC (Microsoft Windows)"
+                ],
+                "tweets": []
+            }
+        ]
+        response = views.main()
+        self.assertIsNotNone(response)
+        self.assertIsInstance(response[0], dict)
--- games/urls.py
+++ games/urls.py
@@ -2,18 +2,19 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from games.views import main, detail, favorite, like, GameViewSet
+from games.views import main, detail, favorite, like, GameViewSet, search
 
 
 app_name = "games"
 
 router = routers.DefaultRouter()
-router.register(r'viewset', GameViewSet)
+router.register(r"viewset", GameViewSet)
 
 urlpatterns = [
     path("", main, name="main"),
     path("like/", like, name="like"),
     path("detail/<int:id>/", detail, name="detail"),
     path("favorite/", favorite, name="favorite"),
-    path('api/', include(router.urls)),
+    path("api/", include(router.urls)),
+    path("search/", search, name='search'),
 ]
